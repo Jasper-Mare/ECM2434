a0b3643e3505d3510b0c61bc937703d2846b879f
rh8090 <rh809@exeter.ac.uk>
2024-02-15 15:01:35 +0000

    map web page update

--- table21 Site/ourSite/map/views.py
+++ table21 Site/ourSite/map/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import Context, loader
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the map index.")
+    template = loader.get_template("map/index.html")
+    return HttpResponse(template.render({},request))
+ 
