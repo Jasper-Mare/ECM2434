5cba479417c180d83b63f2d99138653a5800a481
Jacob Ferris <jacob.ferris33@gmail.com>
2024-02-22 14:01:40 +0000

    tested makeQuiz page

--- table21-Site/ourSite/quizPage/apps.py
+++ table21-Site/ourSite/quizPage/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class HomeConfig(AppConfig):
+class QuizPageConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'home'
+    name = 'quizPage'
--- table21-Site/ourSite/quizPage/views.py
+++ table21-Site/ourSite/quizPage/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
+from django.template import Context, loader
 
 # Create your views here.
 from django.http import HttpResponse
 
-
 def index(request):
-    return HttpResponse("This is the quiz page")
+    template = loader.get_template("newquiz/newquiz.html")
+    return HttpResponse(template.render({}, request))
