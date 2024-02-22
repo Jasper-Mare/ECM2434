f1a0b0dc248a4b2661e8f1f84fecf9a29babd4db
Jasper Mare <jaspermare@icloud.com>
2024-02-22 14:57:24 +0000

    Update urls.py

--- table21-Site/ourSite/contentDB/urls.py
+++ table21-Site/ourSite/contentDB/urls.py
@@ -4,5 +4,16 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("getQuizById", views.getQuizById, name="getQuizById"),
+    path("getLocationById", views.getLocationById, name="getLocationById"),
+    path("getAllLocations", views.getAllLocations, name="getAllLocations"),
+    path("getNearbyLocations", views.getNearbyLocations, name="getNearbyLocations"),
+    path("getQuizzesByLocation", views.getQuizzesByLocation, name="getQuizzesByLocation"),
+    path("createLocation", views.createLocation, name="createLocation"),
+    path("createQuiz", views.createQuiz, name="createQuiz"),
+    path("updateLocation", views.updateLocation, name="updateLocation"),
+    path("updateQuiz", views.updateQuiz, name="updateQuiz"),
+    path("deleteLocation", views.deleteLocation, name="deleteLocation"),
+    path("deleteQuiz", views.deleteQuiz, name="deleteQuiz"),
 ]
 
