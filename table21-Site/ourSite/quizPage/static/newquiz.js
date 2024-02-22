6532a04983b37e1f3c5c67ad45e701bb1f099d8d
Jacob Ferris <jacob.ferris33@gmail.com>
2024-02-22 13:43:17 +0000

    putting Jacobs untested pages

--- table21-Site/ourSite/quizPage/static/newquiz.js
+++ table21-Site/ourSite/quizPage/static/newquiz.js
@@ -4,10 +4,22 @@ o1 = document.getElementById("op1");
 o2 = document.getElementById("op2");
 o3 = document.getElementById("op3");
 
+s1 = document.getElementById("s1");
+s2 = document.getElementById("s2");
+s3 = document.getElementById("s3");
+
 function save(){
     toSave = [question.value, o1.value, o2.value, o3.value];
     question.value = "";
     o1.value = "";
     o2.value = "";
     o3.value = "";
+}
+
+function select(id){
+
+    s1.classList = "choose"
+    s2.classList = "choose"
+    s3.classList = "choose"
+    document.getElementById(id).classList.add("correct")
 }
