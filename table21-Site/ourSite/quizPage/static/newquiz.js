3fdb48cdb7aaaeda6dd4d0f2f909ae95722085f2
Jacob Ferris <jacob.ferris33@gmail.com>
2024-02-23 18:38:24 +0000

    newQuiz writes to db, Quiz reads from db

--- table21-Site/ourSite/quizPage/static/newquiz.js
+++ table21-Site/ourSite/quizPage/static/newquiz.js
@@ -10,33 +10,34 @@ b3 = document.getElementById("b3");
 value = 1;
 
 function save() {
-    alert("oh nooo")
-
-
-    const correctAnswer = document.getElementsByClassName("correct");
-    alert(correctAnswer)
+    correctAnswer = document.querySelector(".correct").id;
+    correctAnswer = parseInt(correctAnswer[1])-1;
     q = question.value
     ans1 = op1.value
     ans2 = op2.value
     ans3 = op3.value
-    correctAns = correctAnswer.value
-    toSave = [q, ans1, ans2, ans3, correctAns];
-
-
+    toSave = [q, ans1, ans2, ans3, correctAnswer];
+    locationID = 1;
+    console.log(toSave);
 
     if (!q || !ans1 || !ans2 || !ans3) {
         alert("something is empty!!!!!")
-
     }
 
 
-    // check valid input
+    const xhr = new XMLHttpRequest();
+    request = 'http://127.0.0.1:8000/contentDB/createQuiz?question='+q+'&answer0='+ans1+'&answer1='+ans2+'&answer2='+ans3+'&correct_answer='+correctAnswer+'&points=10&location_id='+locationID;
+    xhr.open('GET', request, true);
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            const response = JSON.parse(xhr.responseText);
+            console.log(response);
+        }
+    };
+    xhr.send();
 
-    // write to db
 
-
-
-
+    // clear input boxes
     question.value = "";
     op1.value = "";
     op2.value = "";
--- table21-Site/ourSite/quizPage/static/quiz.js
+++ table21-Site/ourSite/quizPage/static/quiz.js
@@ -1,5 +1,5 @@
 // get the elements
-question = document.getElementById("question");
+question = document.getElementById("question_text");
 op1 = document.getElementById("op1");
 op2 = document.getElementById("op2");
 op3 = document.getElementById("op3");
@@ -12,51 +12,41 @@ title.textContent = "Sustainability quiz";
 
 // list of questions in the form where the first one in each is the correct answer
 //questions = [['what is the first char','a','b','c'],['what is the first char','d','e','f'],['what is the first char','g','h','i']];
-questions = getQuestions(1);
 
-if (questions == undefined) {
-  alert("No questions!!!");
-}
-else {
-  questionNumber = 1;
-  scorecount = 0;
-  // load the page when its first created
-  nextquestion();
-  addScore(1, 30); // this gotta go bro
-}
+DoQuiz(0);
 
-questionNumber = 1;
-scorecount = 0;
-// load the page when its first created
-nextquestion();
-addScore(1, 30); // this gotta go bro
-
-function getQuestions(locationID) {
+function DoQuiz(locationID) {
   const xhr = new XMLHttpRequest();
-  xhr.open('GET', 'http://127.0.0.1:8000/contentDB/getQuizzesByLocation?id=' + locationID, true);
+  request = 'http://127.0.0.1:8000/contentDB/getQuizzesByLocation?id=' + locationID
+  console.log(request);
+  xhr.open('GET', request, true);
   xhr.onreadystatechange = function() {
       if (xhr.readyState === 4 && xhr.status === 200) {
           const response = JSON.parse(xhr.responseText);
-          console.log(response);
-          questions = response;
-          return questions;
+          questions = response["quizzes"];
+
+          questionNumber = 1;
+          scorecount = 0;
+          // load the page when its first created
+          nextquestion();
+          addScore(1, 30); // this gotta go bro
       }
   };
+  xhr.send();
 }
 
-function getQuestions() {
-  
-}
+
 
 // update questions and score
 function nextquestion() {
   QN.textContent = "Q"+questionNumber;
   score.textContent = scorecount+"/3";
-  question.textContent = questions[0][0];
+  question.textContent = questions[0]["question"];
+  correct = questions[0]["correct_answer"];
 
   if (questions.length > 0) {
     order = shuffle([0,1,2]);
-    choices = {0: questions[0][1], 1: questions[0][2], 2: questions[0][3]};
+    choices = {0: questions[0]["answer0"], 1: questions[0]["answer1"], 2: questions[0]["answer2"]};
 
     op1.innerHTML = '<input type="radio" name="' + order[0] + '" onclick="choose(name)">' + choices[order[0]];
     op2.innerHTML = '<input type="radio" name="' + order[1] + '" onclick="choose(name)">' + choices[order[1]];
@@ -69,7 +59,7 @@ function nextquestion() {
 
 // on clicking an option increase score if correct then update page with next question
 function choose(name) {
-  if (name == 0) {scorecount ++;}
+  if (name == correct) {scorecount ++;}
   questionNumber ++;
   nextquestion();
 }
