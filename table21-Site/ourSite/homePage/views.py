2b5b96c59ec248ea2a11b6031dd8fbe0eb828f4a
Jasper Mare <jaspermare@icloud.com>
2024-02-23 16:31:33 +0000

    started adding re-direct logic

--- table21-Site/ourSite/homePage/views.py
+++ table21-Site/ourSite/homePage/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import redirect, render
 
 # Create your views here.
 from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world.")
+
+    isLoggedIn = False
+
+    if (isLoggedIn):
+        target_url = '/map'
+    else:
+        target_url = '/login'
+
+
+    return render(request, 'homePage/redirectPage.html', {'target_url': target_url})
+
+
+# redirect user to appropriate page depending on if there is a login cookie yet
+# https://www.geeksforgeeks.org/django-redirects/
