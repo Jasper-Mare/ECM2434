6f45168b00d527f0db6cd79f164c464040a00467
hannahjell <hkj201@exeter.ac.uk>
2024-02-23 11:58:19 +0000

    update newquiz.js

--- table21-Site/ourSite/quizPage/static/newquiz.js
+++ table21-Site/ourSite/quizPage/static/newquiz.js
@@ -1,26 +1,41 @@
 topic = document.getElementById("topic");
 question = document.getElementById("question");
-o1 = document.getElementById("op1");
-o2 = document.getElementById("op2");
-o3 = document.getElementById("op3");
+op1 = document.getElementById("op1");
+op2 = document.getElementById("op2");
+op3 = document.getElementById("op3");
 
-s1 = document.getElementById("s1");
-s2 = document.getElementById("s2");
-s3 = document.getElementById("s3");
+b1 = document.getElementById("s1");
+b2 = document.getElementById("s2");
+b3 = document.getElementById("s3");
 
-function save(){
-    toSave = [question.value, o1.value, o2.value, o3.value];
+function save() {
+    correctAnswer = document.getElementByClass("correct")
+    q = question.value
+    ans1 = op1.value
+    ans2 = op2.value
+    ans3 = op3.value
+    correctAns = correctAnswer.value
+    toSave = [q, ans1, ans2, ans3, correctAns];
+
+    if (q == "" || ans1 == "" || ans2 == "" || ans3 == "" || correctAns == "") {
+
+    }
+
+    // check valid input
 
     // write to db
 
-    // this is a test
+
+
     question.value = "";
-    o1.value = "";
-    o2.value = "";
-    o3.value = "";
+    op1.value = "";
+    op2.value = "";
+    op3.value = "";
+
+
 }
 
-function select(id){
+function select(id) {
     s1.classList = "choose"
     s2.classList = "choose"
     s3.classList = "choose"
