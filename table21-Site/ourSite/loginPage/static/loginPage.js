8c36d06a6a2098cd63afbf10deb8a20208d44e65
hannahjell <hkj201@exeter.ac.uk>
2024-02-26 13:35:43 +0000

    more updates on login and register page
    Added password field and started to check against DB (loginPage and register)

--- table21-Site/ourSite/loginPage/static/loginPage.js
+++ table21-Site/ourSite/loginPage/static/loginPage.js
@@ -1,28 +1,60 @@
-a = document.getElementById("username");
-b = document.getElementById("passwd");
 
 function submitLogin() {
-    alert("hiiii")
-    if (checkIfEmpty(a) || checkIfEmpty(b)) {
+
+    username = (document.getElementById("username").value).trim();
+    password = (document.getElementById("passwd").value).trim();
+
+    if (checkIfEmpty(username) || checkIfEmpty(password)) {
         alert("Please fill in all fields");
         return;
     }
-    alert("before")
 
-    /*
-    const xhr = new XMLHttpRequest();
-    xhr.open('GET', 'http://127.0.0.1:8000/userDB/getUserByName?name=' + a.value, true);
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-            const response = JSON.parse(xhr.responseText);
-            alert("In if ")
-            console.log(response);
-        }
-    };
-    */
-    alert("ufbwefb")
+    checkValidLogin(username);
+
+    setCookie("username", username, 1);
+    // Save password in cookie
+    setCookie("password", password, 1);
+    //alert("cookie is " + getCookie("username"));
+
+
 }
 
 function checkIfEmpty(value) {
-    return (value == "");
+    return (value == null || value == '');
 }
+
+function checkValidLogin(inputUsername, inputPasswd) {
+
+    const xhr = new XMLHttpRequest();
+    request = 'http://127.0.0.1:8000/userDB/getUserByName?name=' + username;
+    xhr.open('GET', request, true);
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            const response = JSON.parse(xhr.responseText);
+            console.log(response);
+            //alert(response);
+            const stringResponse = xhr.responseText;
+            const dbUsername = response.name;
+
+            //check username and password matches ones in DB
+            if (dbUsername == inputUsername) {
+                alert("they match!!!");
+            }
+            //doesn't match any details in system
+            else {
+                alert("they don't");
+            }
+
+        }
+    };
+    xhr.send();
+
+
+
+
+
+    //get username and password from db
+    //check they match
+}
+
+
--- table21-Site/ourSite/loginPage/static/register.js
+++ table21-Site/ourSite/loginPage/static/register.js
@@ -1,35 +1,40 @@
-uname = document.getElementById("username");
-email = document.getElementById("email");
-passwd = document.getElementById("passwd");
-rpasswd = document.getElementById("rptpasswd");
 
 function submitRegisteration() {
 
-    alert("you submitted the form");
+    username = (document.getElementById("username").value).trim();
+    email = (document.getElementById("email").value).trim();
+    passwd = (document.getElementById("passwd").value).trim();
+    rpasswd = (document.getElementById("rptpasswd").value).trim();
 
-    alert("the email is: " + email.value);
-    alert(rpasswd);
 
-    //alert(checkPasswordMatch(passwd.value, rpasswd.value));
-    /*
-    if (checkIfEmpty(a) || checkIfEmpty(b)) {
+    if (checkIfEmpty(username) || checkIfEmpty(email) || checkIfEmpty(passwd) || checkIfEmpty(rpasswd)) {
         alert("Please fill in all fields");
         return;
     }
-    alert("before")
-    */
+
+    checkPasswordMatch(passwd, rpasswd);
+
+    setCookie("username", username, 1);
+    setCookie("email", email, 1);
+
+    alert("cookie is " + getCookie("username"));
+    alert("cookie is " + getCookie("email"));
+
 
 }
 
 function checkIfEmpty(value) {
-    return (value == "");
+    return (value == null || value == "");
 }
+
 
 function checkPasswordMatch(p1, p2) {
     if (p1 == p2) {
+        alert("they match!")
         return true
     }
     else {
+        alert("they don't match!")
         return false
     }
 }
