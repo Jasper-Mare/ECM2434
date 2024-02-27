7a1dbc167c103e2b2a3c63ba7dc4fa444afc0942
M-Fifield <130238758+M-Fifield@users.noreply.github.com>
2024-02-27 12:06:22 +0000

    pushing to branch

---
+++ table21-Site/ourSite/adminPage/static/adminPage.js
@@ -0,0 +1,12 @@
+function display(){
+    element = document.getElementById("display");
+    element.classList.remove("hidden");
+}
+
+function finduser(){
+    username = (document.getElementById("username").value).trim();
+    alert(username);
+
+}
+
+
