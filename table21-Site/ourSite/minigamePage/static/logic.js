0765a209daa23041b81b6fde9edbdc20f6846f13
Jacob Ferris <jacob.ferris33@gmail.com>
2024-03-15 14:45:26 +0000

    set window states

--- table21-Site/ourSite/minigamePage/static/logic.js
+++ table21-Site/ourSite/minigamePage/static/logic.js
@@ -7,7 +7,12 @@ const SCENES = {
     }
 }
 
+
 var currentScene = "game";
+const windowAmount = "5";
+windowStates = [];
+var difficulty = 1;
+
 
 /**
  * no parameters
@@ -16,7 +21,9 @@ var currentScene = "game";
  * Change states of variables
  */
 function logicUpdate() {
-    
+    if (Math.random() > 1-((difficulty/10)+1)) {
+        windowStates[Math.floor(Math.random() * windowAmount)] = 1
+    }
 }
 
 /**
@@ -25,12 +32,15 @@ function logicUpdate() {
  * Preset state of windows, timer, energy meter
  */
 function start() {
-
+    // set all window states to 0
+    for (let i = 0; i < windowAmount; i++) {
+        windowStates[i] = 0
+    }
 }
 
 /**
  * 
  */
 function clickWindow() {
-
+    // Get which window and then reset the state to 0
 }
