f50be1f382a36e914df9ca5b88c365f64f57d05b
Jacob Ferris <jacob.ferris33@gmail.com>
2024-03-18 19:29:20 +0000

    minor changes

--- table21-Site/ourSite/minigamePage/static/logic.js
+++ table21-Site/ourSite/minigamePage/static/logic.js
@@ -5,8 +5,13 @@ export var windowAmount;
 export var windowStates = [];
 export var difficulty;
 export var levelLost = false;
-export var energyWasted = 0;
+export var energyWasted;
 
+const userID = getCookie("login"); // get the userID from the cookie
+if (userID == undefined || userID == "") { // if they are not logged in redirect them to the login page
+  alert("Please login to take the quiz");
+  window.location.href = "/login/";
+}
 
 /**
  * no parameters
@@ -42,6 +47,7 @@ export function start() {
     startScene = "main_menu";
     windowAmount = 36;
     difficulty = 0.03;
+    energyWasted = 0;
 
     // set all window states to 0
     for (let i = 0; i < windowAmount; i++) {
@@ -63,3 +69,34 @@ export function clickWindow(index) {
         SCENES.game.UI.buttons[index].resetColor("#eebb33");
     }
 }
+
+// function to add the score to the user in the database
+function addScore(score) {
+    // get the current score of the user
+    request = 'http://127.0.0.1:8000/userDB/getUserById?id='+String(userID) // get user details from their id
+    getRequest(request)
+    .then(response => {
+      currentscore = parseInt(response["score"]); // get the score attribute from the json
+  
+      // add the score to the current score
+      request = '/userDB/updateUser?id='+String(id)+'&score='+String(score+currentscore) // use updateUser in contentDB
+      getRequest(request)
+      .then(response => {
+        console.log(response);
+      })
+    })
+  }
+
+// function to make a get request and return the response
+async function getRequest(request) {
+    try {
+      const response = await fetch(request);
+      if (!response.ok) {
+        throw new Error('Request failed');
+      }
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error(error);
+    }
+  }
