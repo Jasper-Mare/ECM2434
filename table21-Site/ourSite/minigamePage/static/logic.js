5e121587959d69b1490f05e47ef27dae956bc0e7
Jacob Ferris <jacob.ferris33@gmail.com>
2024-03-18 19:50:50 +0000

    minor changes

--- table21-Site/ourSite/minigamePage/static/logic.js
+++ table21-Site/ourSite/minigamePage/static/logic.js
@@ -101,19 +101,19 @@ async function getRequest(request) {
     }
   }
 
-  function getCookie(cname) {
-    // function to get the cookie of a given name
-        let name = cname + "=";
-        let decodedCookie = decodeURIComponent(document.cookie);
-        let ca = decodedCookie.split(';');
-        for (let i = 0; i < ca.length; i++) {
-            let c = ca[i];
-            while (c.charAt(0) == ' ') {
-                c = c.substring(1);
-            }
-            if (c.indexOf(name) == 0) {
-                return c.substring(name.length, c.length);
-            }
-        }
-        return "";
-    }
+function getCookie(cname) {
+  // function to get the cookie of a given name
+      let name = cname + "=";
+      let decodedCookie = decodeURIComponent(document.cookie);
+      let ca = decodedCookie.split(';');
+      for (let i = 0; i < ca.length; i++) {
+          let c = ca[i];
+          while (c.charAt(0) == ' ') {
+              c = c.substring(1);
+          }
+          if (c.indexOf(name) == 0) {
+              return c.substring(name.length, c.length);
+          }
+      }
+      return "";
+  }
