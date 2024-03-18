cb4c25ccb2e8376a6f1c9a5379e619a5c64dcce1
Jacob Ferris <jacob.ferris33@gmail.com>
2024-03-18 19:49:07 +0000

    cookies working

--- table21-Site/ourSite/minigamePage/static/logic.js
+++ table21-Site/ourSite/minigamePage/static/logic.js
@@ -100,3 +100,20 @@ async function getRequest(request) {
       console.error(error);
     }
   }
+
+  function getCookie(cname) {
+    // function to get the cookie of a given name
+        let name = cname + "=";
+        let decodedCookie = decodeURIComponent(document.cookie);
+        let ca = decodedCookie.split(';');
+        for (let i = 0; i < ca.length; i++) {
+            let c = ca[i];
+            while (c.charAt(0) == ' ') {
+                c = c.substring(1);
+            }
+            if (c.indexOf(name) == 0) {
+                return c.substring(name.length, c.length);
+            }
+        }
+        return "";
+    }
